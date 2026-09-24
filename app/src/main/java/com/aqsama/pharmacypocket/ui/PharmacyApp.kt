d2ef2c33c6acd2b1e26c822632fe0aa09b6c3237
Ahmed Qais <90539519+AQSAMA@users.noreply.github.com>
2026-09-24 20:48:57 +0300

    Fix Compose saveable state holder import

--- app/src/main/java/com/aqsama/pharmacypocket/ui/PharmacyApp.kt
+++ app/src/main/java/com/aqsama/pharmacypocket/ui/PharmacyApp.kt
@@ -17,7 +17,7 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.rememberSaveableStateHolder
+import androidx.compose.runtime.saveable.rememberSaveableStateHolder
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
